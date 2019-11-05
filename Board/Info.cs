e123ebfd040818fd1b9c7ef1c9f664bbf3bbc4ac
TheJohnyKM <56043941+TheJohnyKM@users.noreply.github.com>
2019-11-05 09:20:52 +0200

    version1.1

---
+++ Board/Info.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace Board
+{
+
+    [DataContract]
+    public class Info
+    {
+        [DataMember]
+        public List<RowInfo> List;
+        public Info()
+        {
+            List = new List<RowInfo>();
+
+        }
+        public void Add(string name,int moves,int time)
+        {
+
+            List.Add(new RowInfo(name, moves, time));
+
+
+        }
+        public void Add(RowInfo r)
+        {
+            List.Add(r);
+        }
+        
+    }
+}
